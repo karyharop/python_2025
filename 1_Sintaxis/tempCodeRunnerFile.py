ed794b3157bf6db1024a17527e728e5e7b4cab0b
Kary_H <kityharo@hotmail.com>
2025-10-16 11:42:38 +0200

    segundo commit

--- 1_Sintaxis/tempCodeRunnerFile.py
+++ 1_Sintaxis/tempCodeRunnerFile.py
@@ -1,2 +1,9 @@
-for i in range (5, 0, 1):
-    print(i)
+login("admin@gmail.com", "abcd1234") # False
+
+login("admin@gmail.com", "1234") # True
+
+login("usuario@gmail.com", "1234") # False
+
+# Un ejemplo
+
+if login("admin@gmail.com", "1234"):
